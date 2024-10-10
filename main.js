212dd90677263fdb3ead99abd75f2ceef4cb44f5
MG5-GABRI <lglopez@scl.edu.gt>
2024-10-10 11:08:12 -0600

    componente 1 terminado

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
+import { componente1 } from "./componente/componente1/componente1.js";
 
+let DOM = document.querySelector("#root");
+
+let section1 = document.createElement('section')
+section1.appendChild(componente1())
+
+DOM.appendChild(section1);
